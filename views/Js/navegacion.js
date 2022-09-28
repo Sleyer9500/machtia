ab9eb956807b219ef8e7cd3b3cb877cf428335ae
Sleyer9500 <110828814+Sleyer9500@users.noreply.github.com>
2022-09-28 17:51:23 -0500

    Add files via upload

---
+++ views/Js/navegacion.js
@@ -0,0 +1,5 @@
+function Ocultar_Home_page(){
+    document.getElementById("Home_page").setAttribute("style", "display: none; transition: all 500ms ease;"),
+    document.getElementById("Login").setAttribute("style", "display: flex;")
+
+}
